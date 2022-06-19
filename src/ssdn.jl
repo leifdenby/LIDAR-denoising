911cd9c4c0d4e3b15ac1287321feb393ccb6725f
Leif Denby <leif@denby.eu>
2022-06-19 15:31:25 +0100

    simplify with single halfplane struct

--- src/ssdn.jl
+++ src/ssdn.jl
@@ -1,5 +1,4 @@
-export HalfPlaneOp, DummyHalfPlaneOp, HalfPlaneConv2D, HalfPlaneMaxPool2D
-export offset, calc_padding
+export HalfPlane, halfplane_offset
 
 using Flux: pad_zeros, MaxPool, Conv, SamePad
 
@@ -12,13 +11,11 @@ dimension `dim`
 """
 struct HalfPlane
     op
-    dim
+    dim::Integer
 end
 
-
-function offset(c::HalfPlane)
-    """For a HalfPlaneOp work out what offset is needed to make it half-plane"""
-    stencil_width = c.filter[c.dim]
+"""For a HalfPlaneOp work out what offset is needed to make it half-plane"""
+function halfplane_offset(stencil_width::Integer)
     n_offset = stencil_width ÷ 2
     if stencil_width % 2 == 1
         n_offset += 1
@@ -26,10 +23,23 @@ function offset(c::HalfPlane)
     return n_offset
 end
 
+# calculate half-plane offsets for different operators
+function halfplane_offset(op::Conv, dim::Integer)
+    stencil_width = size(op.weight, 1)
+    return halfplane_offset(stencil_width)
+end
 
-function halfplane_offset(c::HalfPlaneOp, x::AbstractArray{T}) where T
-    """apply offset required for HalfPlaneOp `c` to `x`"""
-    n_offset = offset(c)
+function halfplane_offset(op::MaxPool, dim::Integer)
+    stencil_width = size(op.k, 1)
+    return halfplane_offset(stencil_width)
+end
+
+halfplane_offset(c::HalfPlane) = halfplane_offset(c.op, c.dim)
+
+
+"""calculate halfplane offset"""
+function halfplane_offset(c::HalfPlane, x::AbstractArray{T}) where T
+    n_offset = halfplane_offset(c)
 
     # pad with zeros by the same amount at the beginning
     x_padded = pad_zeros(x, (n_offset, 0), dims=[c.dim])
@@ -39,112 +49,10 @@ function halfplane_offset(c::HalfPlaneOp, x::AbstractArray{T}) where T
     selectdim(x_padded, c.dim, 1:size(x, c.dim))
 end
 
-
-
-
-
-
-abstract type HalfPlaneOp end
-
-function offset(c::HalfPlaneOp, x::AbstractArray{T}) where T
-    """apply offset required for HalfPlaneOp `c` to `x`"""
-    n_offset = offset(c)
-
-    # pad with zeros by the same amount at the beginning
-    x_padded = pad_zeros(x, (n_offset, 0), dims=[c.dim])
-
-    # and crop of last n_offset elements (same as cropping to original size) in
-    # c.dim direction
-    selectdim(x_padded, c.dim, 1:size(x, c.dim))
-end
-
-function offset(c::HalfPlaneOp)
-    """For a HalfPlaneOp work out what offset is needed to make it half-plane"""
-    stencil_width = c.filter[c.dim]
-    n_offset = stencil_width ÷ 2
-    if stencil_width % 2 == 1
-        n_offset += 1
-    end
-    return n_offset
-end
-
-struct DummyHalfPlaneOp <: HalfPlaneOp
-    filter::Tuple{Integer, Integer}
-    dim::Integer
-end
-
-
-# Half-plane convolutions
-
-struct HalfPlaneConv2D <: HalfPlaneOp
-    filter::Tuple{<:Integer,<:Integer}
-    dim
-    conv
-end
-
-function HalfPlaneConv2D(
-    filter::Tuple{<:Integer,<:Integer},
-    ch::Pair{<:Integer,<:Integer},
-    activation::Function;
-    dim=1,
-    pad=0,
-)
-    conv = Conv(filter, ch, activation, pad=pad)
-    HalfPlaneConv2D(size(conv.weight)[1:2], dim, conv)
-end
-
-
-function (c::HalfPlaneConv2D)(x::AbstractArray{T}) where T
-    """
-    apply half-plane convolution. If the size of the convolution
-    in the half-plan direction is an odd number we ensure that
-    the centre of the convolution filter is also excluded
-    """
-    x_offset = offset(c, x)
-    return c.conv(x_offset)
-end
-
-# half-plane max-pooling
-
-
-struct HalfPlaneMaxPool2D <: HalfPlaneOp
-    filter::Tuple{Integer,Integer}
-    dim::Integer
-end
-
-
-function (c::HalfPlaneMaxPool2D)(x::AbstractArray{T}) where T
-    # in Laine et al 2019 they apply a crop before padding with zeros so we'll do the same here
-    d_xlen = size(x, c.dim)
-
-    x_crop = copy(x)
-
-    if c.dim == 1
-        x_crop = @view x[1:d_xlen-1,:,:,:]
-    elseif c.dim == 2
-        x_crop = @view x[:,1:d_xlen-1,:,:]
-    else
-        throw("Not implemented")
-    end
-    n_offset = offset(c)
-
-    # TODO: Laine et al 2019 use 2x2 max-pooling which results in a offset of
-    # 1, I need to work out what to do to generalise this
-    # TODO: Laine et al 2019 use "same"-padding, but in Flux if the shape has
-    # an odd size in a dimension then zeros are added to the beginning in that
-    # dimension which is also where we're adding our own zero-padding. Need to
-    # double-check what happens in tensor flow
-    if n_offset != 1
-        throw(MethodError(n_offset))
-    end
-
-    x_offset = offset(c, x_crop)
-
-    pad = SamePad()
-    mp = MaxPool(c.filter, pad=)(x_offset)
-
-    x_offset = offset(c, x)
-    return c.conv(x_offset)
-
-    return x_mp
+"""
+    (c::HalfPlane)(x)
+Apply half-plane operation to x
+"""
+function (c::HalfPlane)(x::AbstractArray{T}) where T
+    return c.op(halfplane_offset(c, x))
 end
--- test/test_ssdn.jl
+++ test/test_ssdn.jl
@@ -1,6 +1,6 @@
 using Test
-using LIDARdenoising
-using Flux: MaxPool, SamePad
+using LIDARdenoising: HalfPlane, halfplane_offset
+using Flux: MaxPool, SamePad, identity, Conv
 
 
 @testset "half-plane generics" begin
@@ -9,10 +9,12 @@ using Flux: MaxPool, SamePad
     ns = 3  # size of filter is 3x3
     no = (ns + 1) ÷ 2  # expected offset is 2 to avoid the center
     ## half-plane operations
-    d_op = DummyHalfPlaneOp((ns, ns), 1)
+    m = Conv((ns,ns), 1=>1, identity)
+    d_op = HalfPlane(m, 1)
     v1 = randn(Float32, (nx, ny, 1, 1))
-    @test offset(d_op) == no
-    @test all(offset(d_op, v1)[1:no,:] .== 0)
+    @test halfplane_offset(d_op) == no
+    # check that the offsetting is being done correctly
+    @test all(selectdim(halfplane_offset(d_op, v1), 1, 1:no) .== 0)
 end
 
 @testset "half-plane convolutions nx=$nx" for nx in [3, 5]
@@ -21,12 +23,12 @@ end
 
     # check that that only the values in the half-plane contribute to the final value
     v2 = randn(Float32, (nx, ny, 1, 1))
-    offset_conv_xdim = HalfPlaneConv2D((ns,ns),1=>1, identity, pad=0)
-    offset_conv_xdim_pad1 = HalfPlaneConv2D((ns,ns),1=>1, identity, pad=1)
-    offset_conv_xdim_samepad = HalfPlaneConv2D((ns,ns),1=>1, identity, pad=SamePad())
-    offset_conv_ydim = HalfPlaneConv2D((ns,ns),1=>1, identity, pad=0, dim=2)
-    # all operators have same filter width
-    no = offset(offset_conv_xdim)
+    offset_conv_xdim = HalfPlane( Conv((ns,ns),1=>1, identity, pad=0), 1)
+    offset_conv_xdim_pad1 = HalfPlane( Conv((ns,ns),1=>1, identity, pad=1), 1)
+    offset_conv_xdim_samepad = HalfPlane( Conv((ns,ns),1=>1, identity, pad=SamePad()), 1)
+    offset_conv_ydim = HalfPlane( Conv((ns,ns),1=>1, identity, pad=0),2 )
+    # all operators have same stencil size, so we can just calculate the offset once
+    no = halfplane_offset(offset_conv_xdim)
 
     # change the values for the values that sholdn't effect the result
     v2_xmod = copy(v2)
@@ -41,21 +43,21 @@ end
 
     # next we'll check the shapes against just applying the underlying
     # convolutions without offsetting (which creates the half-planes)
-    @test size(offset_conv_xdim(v2)) == size(offset_conv_xdim.conv(v2))
-    @test size(offset_conv_xdim_pad1(v2)) == size(offset_conv_xdim_pad1.conv(v2))
-    @test size(offset_conv_xdim_samepad(v2)) == size(offset_conv_xdim_samepad.conv(v2))
+    @test size(offset_conv_xdim(v2)) == size(offset_conv_xdim.op(v2))
+    @test size(offset_conv_xdim_pad1(v2)) == size(offset_conv_xdim_pad1.op(v2))
+    @test size(offset_conv_xdim_samepad(v2)) == size(offset_conv_xdim_samepad.op(v2))
 end
 
 
-@test_skip @testset "half-plane max-pool" begin
+@testset "half-plane max-pool" begin
     nx = 5
     ny = 4
     ns = 2  # size of filter is 2x2 as in Laine et al 2019
 
-    offset_mp_xdim = HalfPlaneMaxPool2D((ns, ns), 1)
-    offset_mp_ydim = HalfPlaneMaxPool2D((ns, ns), 2)
+    offset_mp_xdim = HalfPlane( MaxPool((ns, ns), pad=SamePad()), 1)
+    offset_mp_ydim = HalfPlane( MaxPool((ns, ns), pad=SamePad()), 2)
     # all operators have same filter width
-    no = offset(offset_mp_xdim)
+    no = halfplane_offset(offset_mp_xdim)
     @test no == 1
 
     # add batch and channel dimensions
@@ -69,7 +71,9 @@ end
     v3_padded_xdim = vcat(repeat([0], 1, ny), v3[1:(nx-1), :]) |> ex
     v3_padded_ydim = hcat(repeat([0], nx, 1), v3[:, 1:(ny-1)]) |> ex
 
+    @test all( halfplane_offset(offset_mp_xdim, v3) .== v3_padded_xdim )
+    @test all( halfplane_offset(offset_mp_ydim, v3) .== v3_padded_ydim )
 
-    @test offset_mp_xdim(v3) == MaxPool((ns, ns))(v3_padded_xdim)
-    @test offset_mp_ydim(v3) == MaxPool((ns, ns))(v3_padded_ydim)
+    @test offset_mp_xdim(v3) == offset_mp_xdim.op(v3_padded_xdim)
+    @test offset_mp_ydim(v3) == offset_mp_ydim.op(v3_padded_ydim)
 end
