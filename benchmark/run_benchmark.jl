145724f219d56aa4667c07d5cbd9096ade9b0f45
Leif Denby <leif@denby.eu>
2022-10-28 12:03:19 +0100

    training opt

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -4,39 +4,45 @@ using Plots
 using MLUtils
 using Flux
 
-
-function (denoiser::LIDARdenoising.Models.Noise2CleanDenoiser)(data::AbstractMatrix{T}) where {T}
-    data_batch_ch = data |> unsqueeze(;dims=3) |> unsqueeze(;dims=4)
-    denoiser(data_batch_ch)[:,:]
-end
-
-
 data = LIDARdenoising.load_data(joinpath(@__DIR__, "../sample-data/qv.x0.0.nc"))[:,:]
+data = LIDARdenoising.load_data(joinpath(@__DIR__, "/mnt/speedy/projects/eurec4a-lidar/LES_data/rico.qv.nc"))
 
 function random_crop(data::AbstractArray{T,2}, N::Int) where {T}
-    nx, ny = size(data)
-    @assert N <= nx && N <= ny
-    @show nx ny
-    i, j = rand(1:(nx-N)), rand(1:(ny-N))
-    return data[i:i+N, j:j+N]
+    nz, nx = size(data)
+    @assert N <= nx && N <= nz
+    i, k = rand(1:(nx-N)), rand(1:(nz-N))
+    return data[k:k+N, i:i+N]
 end
 
-clean_samples = stack([random_crop(data, 26) for i in 1:10], dims=3)
+function random_crop(data::AbstractArray{T,3}, N::Int) where {T}
+    nz, nx, ny = size(data)
+    @assert N <= nx && N <= nz && N <= ny
+    i, j = rand(1:(nx-N)), rand(1:(nz-N))
+    return data[1:N+1, i:i+N, j]
+end
+
+
+N_samples = 1000
+N_size = 26
+clean_samples = stack([random_crop(data, N_size) for i in 1:N_samples], dims=3);
 σ_noise = 0.5
 noisy_samples = LIDARdenoising.add_noise.(clean_samples, σ=σ_noise)
 
-denoiser = Noise2CleanDenoiser(n_layers=2)
-for n in 1:10
-    @info n
-    train!(denoiser, clean_samples, noisy_samples)
-end
+noisy_samples_normed = LIDARdenoising.normalize(noisy_samples)
+clean_samples_normed = LIDARdenoising.normalize(clean_samples, noisy_samples_normed.mean, noisy_samples_normed.std)
+
+denoiser = Noise2CleanDenoiser(n_layers=4) |> gpu
+losses = train!(denoiser, clean_samples_normed, noisy_samples_normed; n_epochs=50, learning_rate=0.1)
+plot(losses)
 denoiser(noisy_samples[:,:,i_sample]) |> cpu
 
-heatmap(random_crop(data, 26), aspectratio=1)
+function plot_sample(denoiser)
+    i_sample = rand(1:size(noisy_samples, 3))
+    plot(
+        heatmap(clean_samples_normed[:,:,i_sample], aspectratio=1),
+        heatmap(noisy_samples_normed[:,:,i_sample], aspectratio=1),
+        heatmap(denoiser(noisy_samples_normed[:,:,i_sample]) |> cpu, aspectratio=1)
+    )
+end
 
-i_sample = rand(1:size(noisy_samples, 3))
-plot(
-    heatmap(clean_samples[:,:,i_sample], aspectratio=1),
-    heatmap(noisy_samples[:,:,i_sample], aspectratio=1),
-    heatmap(denoiser(noisy_samples[:,:,i_sample]) |> cpu, aspectratio=1)
-)
+plot_sample(denoiser)
--- src/models/common.jl
+++ src/models/common.jl
@@ -34,11 +34,11 @@ end
 general purpose data-loaders for denoising applications
 for denoisers using more than one input/output `data` can be a tuple
 """
-function create_dataloader(data; test_valid_fraction=0.9) where {N,T}
+function create_dataloader(data; test_valid_fraction=0.9, batchsize=128) where {N,T}
     data_train, data_test = splitobs(shuffleobs(ensure_has_channel_dim(data)); at=test_valid_fraction)
     
-    dl_train = Flux.DataLoader(data_train)
-    dl_test = Flux.DataLoader(data_test)
+    dl_train = Flux.DataLoader(data_train, batchsize=batchsize)
+    dl_test = Flux.DataLoader(data_test, batchsize=batchsize)
     return dl_train, dl_test
 end
 
@@ -53,35 +53,70 @@ function device(denoiser::AbstractDenoiser)
     end
 end
 
+function early_stopping(losses; N_steps=8)
+    N_total = length(losses)
+    if N_total > N_steps+1
+        maximum(losses[N_total-N_steps-1:N_total-1]) < losses[end]
+    else
+        false
+    end
+end
+
+        
+
 """
 General-purpose training routine for denoiser
 """
-function train!(denoiser::AbstractDenoiser, dl_train::MLUtils.DataLoader, dl_test::MLUtils.DataLoader)
-    opt = Flux.Optimise.Descent(1.0e-4)
+function train!(denoiser::AbstractDenoiser, dl_train::MLUtils.DataLoader, dl_test::MLUtils.DataLoader; n_epochs=10, learning_rate=1.0e-2)
+    @show length(dl_train) length(dl_test)
+
+    opt = Flux.Optimise.Descent(learning_rate)
 
     to_device = device(denoiser)
+    # general loss function with any batch contents calling denoiser specific loss
+    denoiser_loss(batch...) = loss(denoiser, to_device(batch)...)
+    
+    losses = []
+    
+    function evalcb()
+        loss_dl = 0f0
+        for valid_batch in dl_test
+            loss_dl += denoiser_loss(valid_batch...)
+        end
+        avg_loss = loss_dl/length(dl_test)
+        @info "loss" avg_loss
+        push!(losses, avg_loss)
+    end
+    throttled_cb = Flux.throttle(evalcb, 1)
 
-    Flux.train!(
-        # general loss function with any batch contents calling denoiser specific loss
-        (batch...) -> loss(denoiser, to_device(batch)...),
-        Flux.params(denoiser.model),
-        dl_train,
-        opt
-    )
+    for n in 1:n_epochs
+        Flux.train!(
+            denoiser_loss,
+            Flux.params(denoiser.model),
+            dl_train,
+            opt,
+            cb = throttled_cb
+        )
+        if early_stopping(losses)
+            @info "stopped"
+            break
+        end
+    end
+    return losses
 end
 
 """
 Train a denoiser that only requires a single noisy sample
 """
-function train!(denoiser::AbstractDenoiser, noisy_data::AbstractArray{T,N}) where {T,N}
+function train!(denoiser::AbstractDenoiser, noisy_data::AbstractArray{T,N}; kwargs...) where {T,N}
     dl_train, dl_test = create_dataloader(noisy_data)
-    train!(denoiser, dl_train, dl_test)
+    train!(denoiser, dl_train, dl_test; kwargs...)
 end
 
 """
 Train a denoiser that requires clean and noisy inputs
 """
-function train!(denoiser::AbstractDenoiser, noisy_data::AbstractArray{T,N}, clean_data::AbstractArray{T,N}) where {T,N}
+function train!(denoiser::AbstractDenoiser, noisy_data::AbstractArray{T,N}, clean_data::AbstractArray{T,N}; kwargs...) where {T,N}
     dl_train, dl_test = create_dataloader((noisy_data, clean_data))
-    train!(denoiser, dl_train, dl_test)
+    train!(denoiser, dl_train, dl_test; kwargs...)
 end
