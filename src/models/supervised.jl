bfd137d1fe52a964366f5f1156ffddfb39d2bc39
Leif Denby <leif@denby.eu>
2022-10-28 15:36:41 +0100

    add DnCNN

--- src/models/supervised.jl
+++ src/models/supervised.jl
@@ -13,7 +13,7 @@ struct LinearDenoiser <: SupervisedDenoiser
 end
 Flux.@functor LinearDenoiser
 # constructor
-LinearDenoiser() = LinearDenoiser(Conv((1, 1), 1 => 1, pad=0))
+LinearDenoiser(;activation=relu) = LinearDenoiser(Conv((1, 1), 1 => 1, activation, pad=0))
 # inference call
 function (denoiser::LinearDenoiser)(noisy_data::AbstractArray{T,4}) where T
      denoiser.model(noisy_data |> device(denoiser))
@@ -25,9 +25,9 @@ struct Noise2CleanDenoiser <: SupervisedDenoiser
 end
 Flux.@functor Noise2CleanDenoiser
 
-function Noise2CleanDenoiser(;n_layers::Int)
+function Noise2CleanDenoiser(;n_layers::Int, activation=relu)
     model = Chain(
-        [Conv((2, 2), 1=>1) for i in 1:n_layers]...,
+        [Conv((2, 2), 1=>1, activation) for i in 1:n_layers]...,
         [ConvTranspose((2, 2), 1=>1) for i in 1:n_layers]...
     )
     return Noise2CleanDenoiser(model)
@@ -35,4 +35,27 @@ end
 # inference call on trained model
 function (denoiser::Noise2CleanDenoiser)(noisy_data::AbstractArray{T,4}) where T
     denoiser.model(noisy_data |> device(denoiser))
+end
+
+
+"""
+DnCNN: Beyond a Gaussian Denoiser: Residual Learning of Deep CNN for Image Denoising
+Zhang et al 2017
+https://arxiv.org/abs/1608.03981
+
+Based off https://github.com/SaoYan/DnCNN-PyTorch/blob/6b0804951484eadb7f1ea24e8e5c9ede9bea485b/models.py
+"""
+function DnCNN(;kernel_size=3, n_channels=1, n_layers=4, n_features=64, activation=relu)
+    layers = Any[]
+    c_filter = (kernel_size, kernel_size)
+
+    push!(layers, Conv(c_filter, n_channels => n_features, relu, pad=SamePad(), bias=false))
+    for _ in 1:(n_layers-2)
+        push!(layers, Conv(c_filter, n_features => n_features, identity, pad=SamePad(), bias=false))
+        push!(layers, BatchNorm(n_features))
+        push!(layers, relu)
+    end
+    push!(layers, Conv(c_filter, n_features => n_channels, identity, pad=SamePad(), bias=false))
+
+    LinearDenoiser(Chain(layers))
 end
